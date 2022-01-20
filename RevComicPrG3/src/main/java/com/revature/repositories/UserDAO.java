39052d4a172e707f5dbe960b4cbe8dd196820ee2
medinaadvising <94011782+medinaadvising@users.noreply.github.com>
2022-01-20 12:52:20 -0600

    added UserDAO

--- RevComicPrG3/src/main/java/com/revature/repositories/UserDAO.java
+++ RevComicPrG3/src/main/java/com/revature/repositories/UserDAO.java
@@ -1,7 +1,38 @@
 package com.revature.repositories;
 
+import org.hibernate.Session;
+import com.revature.models.User;
+import com.revature.utils.HibernateUtil;
+
 public class UserDAO {
 	
-	//create the DAO once we get Hibernate finished
+public void insertUser(User user) {
+		
+	Session ses = HibernateUtil.getSession();
+		
+	ses.save(user);
+		
+	HibernateUtil.closeSession();
+	}
+	
+public void updateUser(User user) {
+		
+	Session ses = HibernateUtil.getSession();
+		
+	ses.merge(user);
+		
+	HibernateUtil.closeSession();
+		
+	}
+
+public void getUserById(int id) {
+	
+	Session ses = HibernateUtil.getSession();
+	
+	ses.save(id);
+	
+	HibernateUtil.closeSession();
+	
+}
 
 }
