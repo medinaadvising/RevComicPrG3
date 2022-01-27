281e8728f0eadc62ebff7aac58820718878f0c59
Tyler Hodge <t.j.hodge121@gmail.com>
2022-01-27 15:04:12 -0500

    Comps added!

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { DiscountedComponent } from './discounted/discounted.component';
+import { DarkModeComponent } from './dark-mode/dark-mode.component';
+import { FeaturedComponent } from './featured/featured.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { NewUserComponent } from './new-user/new-user.component';
+import { OwnerPanelComponent } from './owner-panel/owner-panel.component';
+import { SalesLogComponent } from './sales-log/sales-log.component';
+import { StockroomComponent } from './stockroom/stockroom.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    CheckoutComponent,
+    DiscountedComponent,
+    DarkModeComponent,
+    FeaturedComponent,
+    LoginComponent,
+    MenuComponent,
+    NewUserComponent,
+    OwnerPanelComponent,
+    SalesLogComponent,
+    StockroomComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontEnd/src/app/sales-log/sales-log.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SalesLogComponent } from './sales-log.component';
+
+describe('SalesLogComponent', () => {
+  let component: SalesLogComponent;
+  let fixture: ComponentFixture<SalesLogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SalesLogComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SalesLogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
